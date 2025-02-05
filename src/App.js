9f5e177d7586d7fabc5987250802cc82eee90fa7
Sandeep <sandeep.chaudhary@infoobjects.com>
2025-02-05 14:09:08 +0530

    confirmation changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import partyPopper from './party-popper.png';
 import subBrandLogo from './sub-brand-logo.svg';
 import glass from './glass.png';
 import { useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import confirmLogo from './confirm.png';
+import axios from 'axios';
 
 function App() {
   const [fullName, setFullName] = useState('');
@@ -16,26 +17,42 @@ function App() {
 
 
   const sendRequest = (e) => {
-    let formData ={
+    let formData = {
       'fullName': fullName,
       'companyName': companyName,
       'jobTitle': jobTitle,
       'workEmail': workEmail,
-      'phoneNumber': phoneNumber
+      'phoneNo': phoneNumber
     }
-    if(fullName && companyName && jobTitle && workEmail){
-      toast("saved successfully.");
+    if (fullName && companyName && jobTitle && workEmail) {
+      axios.post('http://localhost:3030', formData)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      // toast("saved successfully.");
+      document.getElementsByClassName('confirmation-Modal')[0].style.display = 'block';
+      document.getElementsByClassName('landingPage')[0].style.display = 'none';
       console.log('form', formData);
+    }
+  }
+  function hideModal() {
+    setTimeout(() => {
+      document.getElementsByClassName('confirmation-Modal')[0].style.display = 'none';
+      document.getElementsByClassName('landingPage')[0].style.display = '';
       setFullName('');
       setCompanyName('');
       setJobTitle('');
       setWorkEmail('');
       setPhoneNumber('');
-    }
+    }, 100);
   }
+  // hideModal();
   return (
     <div className="App">
-      <div className='row'>
+      <div className='row landingPage'>
         <div className='col-lg-6 col-md-12 image-background hide-sm-logos'>
           <img src={fullBrand} className="full-image" alt="logo" />
         </div>
@@ -58,41 +75,41 @@ function App() {
             <p className='sub-header'>Share your details below fro a chance to win!</p>
 
             {/* <form> */}
-              <div className="container">
-                <div className="row">
-                  <div className="col-sm-12 col-md-6">
-                    <div className="form-group">
-                      <input type="text" className="form-control form-control-lg" value={fullName} onChange={(e) => setFullName(e.target.value)} placeholder="FULL NAME*"></input>
-                    </div>
-                  </div>
-                  <div className="col-sm-12 col-md-6">
-                    <div className="form-group">
-                      <input type="text" className="form-control form-control-lg" value={companyName} onChange={(e) => setCompanyName(e.target.value)} placeholder="COMPANY NAME*" />
-                    </div>
-                  </div>
-                  <div className="col-sm-12 col-md-6">
-                    <div className="form-group">
-                      <input type="text" className="form-control form-control-lg" value={jobTitle} onChange={(e) => setJobTitle(e.target.value)} placeholder="JOB TITLE*" />
-                    </div>
-                  </div>
-                  <div className="col-sm-12 col-md-6">
-                    <div className="form-group">
-                      <input type="text" className="form-control form-control-lg" value={workEmail} onChange={(e) => setWorkEmail(e.target.value)} placeholder="WORK EMAIL*" />
-                    </div>
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-12 col-md-6">
+                  <div className="form-group">
+                    <input type="text" className="form-control form-control-lg" value={fullName} onChange={(e) => setFullName(e.target.value)} placeholder="Full Name*"></input>
                   </div>
                 </div>
-                <div className="row justify-content-md-center">
+                <div className="col-sm-12 col-md-6">
+                  <div className="form-group">
+                    <input type="text" className="form-control form-control-lg" value={companyName} onChange={(e) => setCompanyName(e.target.value)} placeholder="Company Name*" />
+                  </div>
+                </div>
+                <div className="col-sm-12 col-md-6">
+                  <div className="form-group">
+                    <input type="text" className="form-control form-control-lg" value={jobTitle} onChange={(e) => setJobTitle(e.target.value)} placeholder="Job Title*" />
+                  </div>
+                </div>
+                <div className="col-sm-12 col-md-6">
+                  <div className="form-group">
+                    <input type="text" className="form-control form-control-lg" value={workEmail} onChange={(e) => setWorkEmail(e.target.value)} placeholder="Work Email*" />
+                  </div>
+                </div>
+              </div>
+              <div className="row justify-content-md-center">
 
-                  <div className="col-sm-12 col-md-6" style={{ paddingTop: '10px' }}>
-                    <div className="form-group">
-                      <input type="number" className="form-control form-control-lg" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} placeholder="PHONE NUMBER (Optional)" />
-                    </div>
+                <div className="col-sm-12 col-md-6" style={{ paddingTop: '10px' }}>
+                  <div className="form-group">
+                    <input type="number" className="form-control form-control-lg" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} placeholder="Phone Number (Optional)" />
                   </div>
                 </div>
               </div>
-              <div style={{ padding: '20px' }}>
-                <button className="btn btn-primary send-button" onClick={() => sendRequest()}>Send Request</button>
-              </div>
+            </div>
+            <div style={{ padding: '20px' }}>
+              <button className="btn btn-primary send-button" onClick={() => sendRequest()}>Send Request</button>
+            </div>
             {/* </form> */}
           </div>
         </div>
@@ -101,7 +118,22 @@ function App() {
       {/* <div class="footer">
         <p></p>
       </div> */}
-      <ToastContainer />
+      <div className="confirmation-Modal">
+        <div className='confirmation-Content'>
+          <div>
+            <div className='image-background'>
+              <img src={brandlogo} style={{ width: '60vw', marginTop: '35px', marginBottom: '35px' }} alt="logo" />
+            </div>
+            <div>
+              <img src={confirmLogo} style={{ marginTop: '35px' }} alt="logo" />
+            </div>
+          </div>
+          <div style={{ padding: '20px', marginTop: '35px' }}>
+            <button className="btn btn-primary send-button" onClick={() => hideModal()}>close</button>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
